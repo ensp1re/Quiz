5ca7326fafed35a0760f3d39542feedf2c7a319e
ensp1re <ostapuk1318@gmail.com>
2025-03-22 21:23:16 +0200

    trying to fix some frontend bags

--- app/page.tsx
+++ app/page.tsx
@@ -27,10 +27,30 @@ export default function Home() {
   }
 
   if (isLoading) {
+
     return (
       <main className="min-h-screen bg-[#e04bbd] flex flex-col items-center justify-center">
         <div className="flex flex-col items-center">
-          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32 mb-4"></div>
+          <svg
+            className="animate-spin h-8 w-8 text-white mb-4"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
           <h2 className="text-white text-xl font-semibold">Loading...</h2>
           <p className="text-white text-md">Please wait while we prepare everything for you.</p>
         </div>
